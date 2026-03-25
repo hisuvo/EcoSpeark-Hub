0c813a16b4a342e5f5f7af516d2bedb8d68224b9
hisuvo <suvodatta72@gmail.com>
2026-03-25 11:51:51 +0600

    email utils and auth config implement

---
+++ src/config/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { jwt } from "better-auth/plugins";
+import prisma from "./db";
+import { sendVerificationEmail } from "../app/utils/email";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: true,
+    async sendVerificationEmail(data, request) {
+      // data provides { user, url, token }
+      await sendVerificationEmail(data.user.email, data.url, data.token);
+    },
+  },
+  plugins: [jwt()],
+});
