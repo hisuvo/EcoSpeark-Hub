d380799e570eac135f0ef1674c04da8d5670f576
hisuvo <suvodatta72@gmail.com>
2026-03-25 21:59:04 +0600

    env inside added NODE_ENV and app inside include test purpouse use api

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { toNodeHandler } from "better-auth/node";
 import { auth } from "./app/libs/auth";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import { notFound } from "./app/middlewares/notFound";
+import status from "http-status";
 
 const app = express();
 
@@ -12,13 +13,15 @@ app.use(express.json());
 
 // Routes
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to EcoSpark_Hub API" });
+  res
+    .status(status.OK)
+    .json({ success: true, message: "Welcome to EcoSpark_Hub API" });
 });
 
 // Better-Auth handler for authentication routes (/api/auth/*)
 app.all("/api/auth", toNodeHandler(auth));
 
-app.use(globalErrorHandler)
-app.use(notFound)
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
